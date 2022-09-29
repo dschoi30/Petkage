31c4b113bcf08295d47c4e847bc5595af2aa6ee8
dschoi30 <ds0631@gmail.com>
2022-09-29 10:58:52 +0900

    20220929 STS 설정

--- PETKAGE/petkage/src/main/java/com/finalproject/petkage/common/util/MultipartFileUtil.java
+++ PETKAGE/petkage/src/main/java/com/finalproject/petkage/common/util/MultipartFileUtil.java
@@ -11,40 +11,40 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class MultipartFileUtil {
-   public static String save(MultipartFile upfile, String location) {
-      String renamedFileName = null;
-      String originalFileName = upfile.getOriginalFilename();
-      
-      log.info("Upfile Name : {}", originalFileName);
-      log.info("location : {}", location);
-      
-      File folder = new File(location);
-      
-      if(!folder.exists()) {
-         folder.mkdirs();
-      }
-      
-      renamedFileName = 
-            LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmssSSS")) + 
-            originalFileName.substring(originalFileName.lastIndexOf("."));
-      
-      try {
-         upfile.transferTo(new File(location + "/" + renamedFileName));
-         
-      } catch (IllegalStateException | IOException e) {
-         e.printStackTrace();
-      }
-      
-      return renamedFileName;
-   }
+	public static String save(MultipartFile upfile, String location) {
+		String renamedFileName = null;
+		String originalFileName = upfile.getOriginalFilename();
+		
+		log.info("Upfile Name : {}", originalFileName);
+		log.info("location : {}", location);
+		
+		File folder = new File(location);
+		
+		if(!folder.exists()) {
+			folder.mkdirs();
+		}
+		
+		renamedFileName = 
+				LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmssSSS")) + 
+				originalFileName.substring(originalFileName.lastIndexOf("."));
+		
+		try {
+			upfile.transferTo(new File(location + "/" + renamedFileName));
+			
+		} catch (IllegalStateException | IOException e) {
+			e.printStackTrace();
+		}
+		
+		return renamedFileName;
+	}
 
-   public static void delete(String location) {
-      log.info("location : {}", location);
-      
-      File file = new File(location);
-      
-      if(file.exists()) {
-         file.delete();
-      }
-   }
+	public static void delete(String location) {
+		log.info("location : {}", location);
+		
+		File file = new File(location);
+		
+		if(file.exists()) {
+			file.delete();
+		}
+	}
 }
